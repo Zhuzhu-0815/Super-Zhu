cc3ee70b6d45496cffbb8741dc1cfb2e7328a678
Zhuzhu-0815 <1572323786@qq.com>
2022-09-03 21:20:02 +0800

    删除了catch中的测试语句

--- src/main/java/com/mark/mysql_demo/Service/MysqlService.java
+++ src/main/java/com/mark/mysql_demo/Service/MysqlService.java
@@ -88,7 +88,6 @@ public class MysqlService {
             });
             baseResponse.setData(res);
         } catch (Exception e) {
-            System.out.println("张弛");
             System.out.println(e);
         }
         return baseResponse;
