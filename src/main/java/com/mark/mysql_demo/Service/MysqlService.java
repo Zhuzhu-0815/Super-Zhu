d86d226e679c9a435ad2a83e93e05909f219e300
Zhuzhu-0815 <1572323786@qq.com>
2022-09-03 21:12:44 +0800

    hello world

--- src/main/java/com/mark/mysql_demo/Service/MysqlService.java
+++ src/main/java/com/mark/mysql_demo/Service/MysqlService.java
@@ -88,7 +88,7 @@ public class MysqlService {
             });
             baseResponse.setData(res);
         } catch (Exception e) {
-            System.out.println(1);
+            System.out.println(123456789);
             System.out.println(e);
         }
         return baseResponse;
